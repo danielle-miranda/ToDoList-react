2edd838d60c90d4442c4f3d728d7182793110d4c
Danielle Miranda <danielle.m.miranda96@gmail.com>
2025-02-19 16:01:20 -0800

    create new tasks

--- script.js
+++ script.js
@@ -40,9 +40,14 @@ class ToDoList extends React.Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.fetchTasks = this.fetchTasks.bind(this);
   }
 
   componentDidMount() {
+    this.fetchTasks();  // get tasks on mount
+  }
+  fetchTasks() {
+    // move the get tasks code into its own method so we can use it at other places
     fetch("https://fewd-todolist-api.onrender.com/tasks?api_key=48")
       .then(checkStatus)
       .then(json)
@@ -61,9 +66,31 @@ class ToDoList extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    // do nothing for now
+    let { new_task } = this.state;
+    new_task = new_task.trim();
+    if (!new_task) {
+      return;
+    }
+    fetch("https://fewd-todolist-api.onrender.com/tasks?api_key=48", {
+      method: "POST",
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        task: {
+          content: new_task
+        }
+      }),
+    }).then(checkStatus)
+      .then(json)
+      .then((data) => {
+        this.setState({new_task: ''});
+        this.fetchTasks();
+      })
+      .catch((error) => {
+        this.setState({ error: error.message });
+        console.log(error);
+      })
   }
-
   render() {
     const { new_task, tasks } = this.state;
 
